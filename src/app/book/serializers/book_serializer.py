68f85a1c12ee53dea7a2d64d73a53918942d944c
Steven SHIH <steven.shih@tao-rui.com>
2026-04-16 16:58:32 +0800

    Use Viewset version.

--- src/app/book/serializers/book_serializer.py
+++ src/app/book/serializers/book_serializer.py
@@ -1,43 +1,47 @@
+from django.utils import timezone
 from rest_framework.serializers import ModelSerializer
 
 from app.book.models.book_model import Book
 from app.book.services.book_service import BookService
 
-
 class BookSerializer(ModelSerializer):
 
     class Meta:
         model = Book
-        fields = [
-            "id",
-            "book_name",
-            "description",
-            "writer",
-            "publish_date",
-            "created_at",
-            "updated_at",
-        ]
+        fields = "__all__"
 
 
 class BookCreateSerializer(ModelSerializer):
 
     class Meta:
         model = Book
-        fields = [
-            "book_name",
-            "description",
-            "writer",
-            "publish_date",
-        ]
+        fields = ("book_name", "description", "writer", "publish_date")
+
+    def validate(self, attrs):
+        BookService.validate_book_name_unique(attrs["book_name"])
+        return attrs
+
+    def create(self, validated_data):
+        validated_data["created_at"] = timezone.now()
+        return Book.objects.create(**validated_data)
 
 
 class BookUpdateSerializer(ModelSerializer):
 
     class Meta:
         model = Book
-        fields = [
-            "book_name",
-            "description",
-            "writer",
-            "publish_date",
-        ]
+        fields = ("book_name", "description", "writer", "publish_date")
+
+    def validate(self, attrs):
+        BookService.validate_book_name_unique(
+            attrs["book_name"],
+            exclude_id=self.instance.id,
+        )
+        return attrs
+
+    def update(self, instance, validated_data):
+        validated_data["updated_at"] = timezone.now()
+        for field, value in validated_data.items():
+            setattr(instance, field, value)
+        instance.save()
+        return instance
